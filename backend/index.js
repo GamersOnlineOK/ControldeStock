85df1566aceede0488b6cd0b8e07fbec88c5e88f
Leonardo De Los Santos <leo_dls@hotmail.com>
2025-11-10 13:05:56 -0300

    CORS http

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'https://simi-pry.com.ar',
-    'https://www.simi-pry.com.ar'
+    'http://simi-pry.com.ar',
+    'https://www.simi-pry.com.ar',
+    'http://www.simi-pry.com.ar'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
