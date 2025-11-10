d2b0f01ed9f2756203089010163551f85233c2b7
Leonardo De Los Santos <leo_dls@hotmail.com>
2025-11-10 13:09:11 -0300

    Cambio de rutas

--- frontend/src/utils/apiUrl.js
+++ frontend/src/utils/apiUrl.js
@@ -2,7 +2,7 @@ const baseURL =()=> {
     if (typeof window !== 'undefined' && 
       !window.location.hostname.includes('localhost') &&
       !window.location.hostname.includes('127.0.0.1')) {
-    return 'http://simi-pry.com.ar:3200/api/';
+    return 'https://simi-pry.com.ar:3200/api/';
   }
   return 'http://localhost:3200/api/';
 }
