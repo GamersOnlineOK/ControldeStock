55618e7ec6b0a40a1cd257db731207f1de1ccbad
Leonardo De Los Santos <leo_dls@hotmail.com>
2025-11-10 12:56:57 -0300

    Solucion a rutras front

--- frontend/src/utils/apiUrl.js
+++ frontend/src/utils/apiUrl.js
@@ -2,7 +2,7 @@ const baseURL =()=> {
     if (typeof window !== 'undefined' && 
       !window.location.hostname.includes('localhost') &&
       !window.location.hostname.includes('127.0.0.1')) {
-    return 'https://simi-pry.com.ar/api/';
+    return 'http://simi-pry.com.ar:3200/api/';
   }
   return 'http://localhost:3200/api/';
 }
