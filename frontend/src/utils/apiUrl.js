2608800d74947adae3bd8d46696a8d039329b2c4
Leonardo De Los Santos <leo_dls@hotmail.com>
2025-11-10 13:29:00 -0300

    URL relativas en el front

--- frontend/src/utils/apiUrl.js
+++ frontend/src/utils/apiUrl.js
@@ -2,9 +2,9 @@ const baseURL =()=> {
     if (typeof window !== 'undefined' && 
       !window.location.hostname.includes('localhost') &&
       !window.location.hostname.includes('127.0.0.1')) {
-    return 'https://simi-pry.com.ar:3200/api/';
+    return '/api/';
   }
-  return 'http://localhost:3200/api/';
+  return '/api/';
 }
 const URLJSON = {
   baseURL: baseURL()
